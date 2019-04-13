0a0c867750508d04724e0a60a53adaf61acb9111
Mario Ancevski <marioancevski25@gmail.com>
2019-04-13 19:13:00 +0200

    commited

---
+++ Newfolder/Kolokviumska/Airplanes/addDest.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Airplanes
+{
+    partial class addDest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nudKm = new System.Windows.Forms.NumericUpDown();
+            this.tbIme = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nudCena = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudKm)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCena)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nudKm
+            // 
+            this.nudKm.Location = new System.Drawing.Point(12, 80);
+            this.nudKm.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.nudKm.Name = "nudKm";
+            this.nudKm.Size = new System.Drawing.Size(212, 20);
+            this.nudKm.TabIndex = 1;
+            // 
+            // tbIme
+            // 
+            this.tbIme.Location = new System.Drawing.Point(12, 25);
+            this.tbIme.Name = "tbIme";
+            this.tbIme.Size = new System.Drawing.Size(212, 20);
+            this.tbIme.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Име";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 179);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(87, 35);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Додади";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(125, 179);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(87, 35);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "Откажи";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Километри";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Цена";
+            // 
+            // nudCena
+            // 
+            this.nudCena.Location = new System.Drawing.Point(12, 130);
+            this.nudCena.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.nudCena.Name = "nudCena";
+            this.nudCena.Size = new System.Drawing.Size(212, 20);
+            this.nudCena.TabIndex = 2;
+            // 
+            // addDest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(288, 226);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nudCena);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbIme);
+            this.Controls.Add(this.nudKm);
+            this.Name = "addDest";
+            this.Text = "addDest";
+            ((System.ComponentModel.ISupportInitialize)(this.nudKm)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudCena)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown nudKm;
+        private System.Windows.Forms.TextBox tbIme;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown nudCena;
+    }
+}
